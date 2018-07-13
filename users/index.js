242b8dc341f3691fffa317298451d2ad2e55f6be
Vennie Chang <venniechang@yahoo.com>
2018-07-13 00:14:22 -0700

    first commit

---
+++ users/index.js
@@ -0,0 +1,5 @@
+'use strict';
+const {User} = require('./User');
+const {router} = require('./userRouter');
+
+module.exports = {User, router};
---
+++ users/userRouter.js
@@ -0,0 +1,160 @@
+'use strict';
+const express = require('express');
+const bodyParser = require('body-parser');
+const passport = require('passport');
+
+const {User} = require('./User');
+
+const router = express.Router();
+
+const jsonParser = bodyParser.json();
+
+const jwtAuth = passport.authenticate('jwt', {session: false});
+
+// Post to register a new user
+router.post('/', jsonParser, (req, res) => {
+  console.log(req.body);
+  const stringFields = ['username', 'password', 'firstName', 'lastName'];
+  const nonStringField = stringFields.find(
+    field => field in req.body && typeof req.body[field] !== 'string'
+  );
+
+  if (nonStringField) {
+    return res.status(422).json({
+      code: 422,
+      reason: 'ValidationError',
+      message: 'Incorrect field type: expected string',
+      location: nonStringField
+    });
+  }
+
+  const explicityTrimmedFields = ['username', 'password', 'firstName', 'lastName'];
+  const nonTrimmedField = explicityTrimmedFields.find(
+    field => req.body[field].trim() !== req.body[field]
+  );
+
+  if (nonTrimmedField) {
+    return res.status(422).json({
+      code: 422,
+      reason: 'ValidationError',
+      message: 'Cannot start or end with whitespace',
+      location: nonTrimmedField
+    });
+  }
+
+  const sizedFields = {
+    username: {
+      min: 1
+    },
+    password: {
+      min: 5,
+      // bcrypt truncates after 72 characters, so let's not give the illusion
+      // of security by storing extra (unused) info
+      max: 72
+    }
+  };
+  const tooSmallField = Object.keys(sizedFields).find(
+    field =>
+      'min' in sizedFields[field] &&
+            req.body[field].trim().length < sizedFields[field].min
+  );
+  const tooLargeField = Object.keys(sizedFields).find(
+    field =>
+      'max' in sizedFields[field] &&
+            req.body[field].trim().length > sizedFields[field].max
+  );
+
+  if (tooSmallField || tooLargeField) {
+    return res.status(422).json({
+      code: 422,
+      reason: 'ValidationError',
+      message: tooSmallField
+        ? `Must be at least ${sizedFields[tooSmallField]
+          .min} characters long`
+        : `Must be at most ${sizedFields[tooLargeField]
+          .max} characters long`,
+      location: tooSmallField || tooLargeField
+    });
+  }
+
+  let {username, password, firstName = '', lastName = ''} = req.body;
+
+  return User.find({username})
+    .count()
+    .then(count => {
+      if (count > 0) {
+        // There is an existing user with the same username
+        return Promise.reject({
+          code: 422,
+          reason: 'ValidationError',
+          message: 'Username already taken. Please try again.',
+          location: 'username'
+        });
+      }
+      // If there is no existing user, hash the password
+      return User.hashPassword(password);
+    })
+    .then(hash => {
+      return User.create({
+        username,
+        password: hash,
+        firstName,
+        lastName
+      });
+    })
+    .then(user => {
+      return res.status(201).json(user.serialize());
+    })
+    .catch(err => {
+      // Forward validation errors on to the client, otherwise give a 500
+      // error because something unexpected has happened
+      if (err.reason === 'ValidationError') {
+        return res.status(err.code).json(err);
+      }
+      res.status(500).json({code: 500, message: 'Internal server error'});
+    });
+
+
+// Never expose all your users like below in a prod application
+// we're just doing this so we have a quick way to see
+// if we're creating users. keep in mind, you can also
+// verify this in the Mongo shell.
+})
+
+router.delete('/api/user/:id', (req, res) =>{
+  User.findByIdAndRemove(req.params.id)
+  .then(() => {
+    res.status(204).json({ message: 'success' });
+  })
+  .catch(err => {
+    console.error(err);
+    res.status(500).json({ error: 'something went terribly wrong' });
+  });
+})
+
+router.put('/', jsonParser, jwtAuth, (req, res) => {
+
+
+  User.hashPassword(req.body.password)
+  .then (hash => {
+    
+    const updateUser = {
+      password: hash
+    }
+
+    User
+      .findOneAndUpdate({username: req.user.username}, {$set: updateUser}, {new: true})
+      .then(updatedPost => res.status(204).end())
+      .catch(err => res.status(500).json({ message: 'Something went wrong' }));
+  })
+})
+
+  
+router.get('/', (req, res) => {
+  return User.find()
+    .then(users => res.json(users.map(user => user.serialize())))
+    .catch(err => res.status(500).json({message: 'Internal server error'}));
+});
+
+
+module.exports = {router};
